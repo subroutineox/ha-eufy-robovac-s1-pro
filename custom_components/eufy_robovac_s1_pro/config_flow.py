46761809694a9c26ec4b8ff9278698922844ce44
subroutineox <65804196+subroutineox@users.noreply.github.com>
2026-03-25 00:19:33 +0100

    Update config_flow.py

--- custom_components/eufy_robovac_s1_pro/config_flow.py
+++ custom_components/eufy_robovac_s1_pro/config_flow.py
@@ -5,12 +5,17 @@ from homeassistant import data_entry_flow
 from homeassistant.config_entries import ConfigFlow
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
 
-from .const import DOMAIN
+from .const import DOMAIN, CONF_IP_ADDRESS
 from .eufy_local_id_grabber.clients import EufyHomeSession
 
 logger = logging.getLogger(__name__)
 
-EUFY_LOGIN_SCHEMA = vol.Schema({vol.Required("username"): str, vol.Required("password"): str})
+# Neues Schema mit optionalem IP-Feld
+EUFY_LOGIN_SCHEMA = vol.Schema({
+    vol.Required("username"): str, 
+    vol.Required("password"): str,
+    vol.Optional(CONF_IP_ADDRESS, default=""): str  # HIER NEU
+})
 
 
 class EufyVacuumConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -20,6 +25,7 @@ class EufyVacuumConfigFlow(ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             username = user_input["username"]
             password = user_input["password"]
+            ip_address = user_input.get(CONF_IP_ADDRESS, "").strip() # HIER NEU
 
             await self.async_set_unique_id(username)
             self._abort_if_unique_id_configured()
@@ -32,11 +38,16 @@ class EufyVacuumConfigFlow(ConfigFlow, domain=DOMAIN):
                 logger.exception("Error when logging in with %s", username)
 
                 # TODO: proper exception handling
-                errors["username"] = errors["password"] = "Username or password is incorrect"
+                errors["base"] = "Username or password is incorrect"
             else:
                 return self.async_create_entry(
                     title=username,
-                    data={CONF_EMAIL: username, CONF_PASSWORD: password},
+                    # HIER NEU: Wir speichern die IP mit in der Config
+                    data={
+                        CONF_EMAIL: username, 
+                        CONF_PASSWORD: password, 
+                        CONF_IP_ADDRESS: ip_address
+                    },
                 )
 
-        return self.async_show_form(step_id="user", data_schema=EUFY_LOGIN_SCHEMA)
+        return self.async_show_form(step_id="user", data_schema=EUFY_LOGIN_SCHEMA, errors=errors)
